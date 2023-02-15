f813c92a09d15247b0cfcbd006fdd34c5f104a9e
angelajiaqichen <57929028+angelajiaqichen@users.noreply.github.com>
2023-02-14 21:52:02 -0500

    Create javascript.js

---
+++ solution-hw3/js/javascript.js
@@ -0,0 +1,37 @@
+const glazingOptions = [
+    {
+      glazingOption: 'Keep original',
+      priceAdaptation: 0,
+    },
+    {
+      glazingOption: 'Sugar milk',
+      priceAdaptation: 0,
+    },
+    {
+      glazingOption: 'Vanilla milk',
+      priceAdaptation: 0.5,
+    },
+    {
+      glazingOption: 'Double chocolate',
+      priceAdaptation: 1.5,
+    },
+];
+
+const packSizeOptions = [
+    {
+      packSizeOption: '1',
+      priceAdaptation: 1,
+    },
+    {
+      packSizeOption: '3',
+      priceAdaptation: 3,
+    },
+    {
+      packSizeOption: '6',
+      priceAdaptation: 5,
+    },
+    {
+      packSizeOption: '12',
+      priceAdaptation: 10,
+    },
+];
