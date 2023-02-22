93df0248c79092a5f30856803b830acfc3e7a2da
angelajiaqichen <57929028+angelajiaqichen@users.noreply.github.com>
2023-02-22 16:17:59 -0500

    created js functions to fetch selected item and add to cart

--- solution-hw4/js/javascript.js
+++ solution-hw4/js/javascript.js
@@ -46,30 +46,80 @@ for (let i = 0; i < glazingOptions.length; i++){
     option.value = glazingOptions[i].priceAdaptation; 
     const select = document.querySelector('#glazingDropdown'); 
     select.appendChild(option);
-  }
+}
 
-  for (let i = 0; i < packSizeOptions.length; i++){
+for (let i = 0; i < packSizeOptions.length; i++){
     var option = document.createElement('option');
     option.text = packSizeOptions[i].packSizeOption; 
     option.value = packSizeOptions[i].priceAdaptation; 
     const select = document.querySelector('#packDropdown'); 
     select.appendChild(option);
-  }
+}
 
 
-  function glazingTrigger(element){
-    const displayPrice = document.querySelector('.detailTotalPrice');
-    const glazingChange = parseFloat(element.value); 
-    const numPacks = document.querySelector('#packDropdown').value; 
-    const currentPrice = (2.49 + glazingChange) * numPacks; 
-    displayPrice.innerText = '$ ' + parseFloat(currentPrice).toFixed(2); //keeps two decimal places
-    // https://www.w3schools.com/jsref/jsref_tofixed.asp
-  }
+function glazingTrigger(element){
+  const displayPrice = document.querySelector('.detailTotalPrice');
+  const glazingChange = parseFloat(element.value); 
+  const numPacks = document.querySelector('#packDropdown').value; 
+  const currentPrice = (2.49 + glazingChange) * numPacks; 
+  displayPrice.innerText = '$ ' + parseFloat(currentPrice).toFixed(2); //keeps two decimal places
+  // https://www.w3schools.com/jsref/jsref_tofixed.asp
+}
   
-  function packTrigger(element){
-    const displayPrice = document.querySelector('.detailTotalPrice');
-    const packChange = element.value; 
-    const glazingPrice = document.querySelector('#glazingDropdown').value; 
-    const currentPrice = (2.49 + parseFloat(glazingPrice)) * packChange; 
-    displayPrice.innerText = '$ ' + parseFloat(currentPrice).toFixed(2); 
+function packTrigger(element){
+  const displayPrice = document.querySelector('.detailTotalPrice');
+  const packChange = element.value; 
+  const glazingPrice = document.querySelector('#glazingDropdown').value; 
+  const currentPrice = (2.49 + parseFloat(glazingPrice)) * packChange; 
+  displayPrice.innerText = '$ ' + parseFloat(currentPrice).toFixed(2); 
+}
+
+// HW 4
+
+class Roll {
+  constructor(rollType, rollGlazing, packSize, basePrice) {
+    this.type = rollType;
+    this.glazing =  rollGlazing;
+    this.size = packSize;
+    this.basePrice = basePrice;
   }
+}
+  
+// get selected item 
+let glazeElement = document.querySelector('#glazingDropdown'); 
+let packElement = document.querySelector('#packDropdown');
+
+const imageFile = document.querySelector(".product-img");
+imageFile.src = 'products/' + rollType + '-cinnamon-roll.jpg';
+
+// URL
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const rollType = params.get("roll");
+
+const headerElement = document.querySelector("#title");
+headerElement.innerText = rollType +' Cinnamon Roll';
+
+let basePrice = rolls[rollType].basePrice; // get baseprice from rollsData
+let cart = []; // init cart
+
+
+function addToCart(){
+  const addRoll = new Roll(rollType, rollGlazing, packSize, basePrice);
+  cart.push(addRoll);
+  console.log(cart);
+  displayTotal();
+}
+
+
+const addToCartButton = document.querySelector('#cartbutton .highlight');
+addToCartButton.onclick = addToCart;
+console.log(addToCartButton)
+
+
+// Initially, display the price with the default settings
+let glazingPrice = glazingOptions[0].priceAdaptation;
+let rollGlazing = glazingOptions[0].glazingOption;
+let packPrice = packSizeOptions[0].priceAdaptation;
+let packSize = packSizeOptions[0].packSizeOption;
+displayTotal();
