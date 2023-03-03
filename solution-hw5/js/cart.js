63c19758e1ff6f1fa0f7750fde3704013bafe6b0
angelajiaqichen <57929028+angelajiaqichen@users.noreply.github.com>
2023-03-03 15:23:19 -0500

    fixed bug with cart bottom display

--- solution-hw5/js/cart.js
+++ solution-hw5/js/cart.js
@@ -60,7 +60,7 @@ const rollB = addRollToCart("Walnut", "Vanilla milk", 12, 3.49);
 const rollC = addRollToCart("Raisin", "Sugar milk", 3, 2.99); 
 const rollD = addRollToCart("Apple", "Keep original", 3, 3.49);
 
-let currentCartTotal = 0; 
+let cartCurrTotal = 0; 
 
 for (const roll of cart){
   createElement(roll); 
@@ -76,43 +76,38 @@ function deleteRoll(roll){
 function createElement(roll){
   const template = document.querySelector('#carttemplate')
   const clone = template.content.cloneNode(true); 
-
   roll.element = clone.querySelector('.cartitem');
   updateElement(roll); 
 
+  // remove item from cart
   const removeBtn = roll.element.querySelector('.remove'); 
   removeBtn.addEventListener('click', () => {
+    // cannot delete if cart is already empty
     if(cart.length != 0){
       deleteRoll(roll);
-      currentCartTotal -= parseFloat(calculateCartTotal(roll)); 
-      const cartTotalElement = document.querySelector('.carttotal'); 
-      cartTotalElement.innerText = '$ ' + currentCartTotal.toFixed(2);
+      cartCurrTotal -= parseFloat(calculateCartTotal(roll)); 
+      const cartTotalPrice = document.querySelector('.carttotal'); 
+      cartTotalPrice.innerText = '$ ' + cartCurrTotal.toFixed(2);
     }
   });
-
-
+  // cart is not empty
   const rollListElement = document.querySelector('.cartitems'); 
   rollListElement.append(roll.element)
-
-  currentCartTotal += parseFloat(calculateCartTotal(roll)); 
-  const cartTotalElement = document.querySelector('.carttotal'); 
-  cartTotalElement.innerText = '$ ' + currentCartTotal.toFixed(2);
+  cartCurrTotal += parseFloat(calculateCartTotal(roll)); 
+  const cartTotalPrice = document.querySelector('.carttotal'); 
+  cartTotalPrice.innerText = '$ ' + cartCurrTotal.toFixed(2);
 }
 
-
+// update cart given the rolls
 function updateElement(roll){
   const rollImageElement = roll.element.querySelector('.cartimage'); 
   rollImageElement.src = './products/' + roll.type.toLowerCase() + '-cinnamon-roll.jpg'; 
-
   const rollTypeElement = roll.element.querySelector('#type'); 
   rollTypeElement.innerText = roll.type + ' Cinnamon Roll'; 
-
   const rollGlazingElement = roll.element.querySelector('#glazing'); 
   rollGlazingElement.innerText = 'Glazing: ' + roll.glazing; 
-
   const rollPackSizeElement = roll.element.querySelector('#pack-size');
   rollPackSizeElement.innerText = 'Pack Size: ' + roll.size; 
-
   const itemPrice = roll.element.querySelector('.cartproductprice'); 
   itemPrice.innerText = '$ ' + calculateCartTotal(roll); 
 }
